3e90ac455eef792f167086b37ca846c2e11d59da
Joe Fiorini <joe@joefiorini.com>
2019-09-25 14:14:56 -0400

    Update test case with some more examples

--- test.tsx
+++ test.tsx
@@ -5,8 +5,31 @@ type Props<T1, T2> = {
   doo: T2;
 };
 
-const TestComponent = <T1, T2>({  }: Props<T1, T2>) => {
+function TestComponent({}: Props) {
   return <>TestComponent</>;
-};
+}
+
+function TestComponent<T1, T2>({}: Props<T1, T2>) {
+  return <>TestComponent</>;
+}
+
+const TestComponent = <T1 extends {}>(type: T1)  => (
+	props: Props<T1, T2>
+) => {
+  return <>TestComponent</>;
+}
+
+const blah = doo()
+
+const valueTypeGuard = <T extends Props['type']>(type: T) => (
+  props: Props | FormattedValueProps,
+): props is PropsFromValueType<T> => props.type === type;
+
+const isDateValue = valueTypeGuard('date');
+const isAddressValue = valueTypeGuard('address');
+const isListValue = valueTypeGuard('list');
+const isTextValue = valueTypeGuard('text');
+
+
 
 export default TestComponent;
